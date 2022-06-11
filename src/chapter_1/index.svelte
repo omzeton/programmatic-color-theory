51e0b7cb3aa0ec3f94de8f50e6dd3e199e8a773c
Adam Rolczyk <adam.rolczyk@idea.edu.pl>
2022-06-11 21:45:13 +0200

    Get through chapter 1

--- src/chapter_1/index.svelte
+++ src/chapter_1/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { formatHex } from 'culori'
     import type { BaseColor, Types } from './types'
 
     const adjustHue = (val: number): number => {
@@ -17,12 +18,12 @@
             splitComplementary: [0, 150, 210]
         };
 
-        const palettes: Record<Types, BaseColor> = {
-            analogous: { c: 0, l: 0, h: 0, mode: 'lch' },
-            complementary: { c: 0, l: 0, h: 0, mode: 'lch' },
-            splitComplementary: { c: 0, l: 0, h: 0, mode: 'lch' },
-            tetradic: { c: 0, l: 0, h: 0, mode: 'lch' },
-            triadic: { c: 0, l: 0, h: 0, mode: 'lch' }
+        const palettes: Record<Types, BaseColor[]> = {
+            analogous: [],
+            complementary: [],
+            splitComplementary: [],
+            tetradic: [],
+            triadic: []
         }
 
         for (const type of Object.keys(targetHueSteps)) {
@@ -36,8 +37,50 @@
 
         return palettes
     }
+
+    const someBaseColor: BaseColor = {
+        l: 50 + Math.random() * 10,
+        c: 60 + Math.random() * 10,
+        h: Math.random() * 360,
+        mode: 'lch'
+    }
+
+    const palettes = createPalettes(someBaseColor)
+    const triad = palettes.splitComplementary.map((colorLCH) => formatHex(colorLCH))
+
+    console.log({
+        palettes,
+        triad
+    })
 </script>
 
+<style>
+    .chapter-two {
+        width: 100%;
+        height: 400px;
+        border: 1px solid #000;
+        padding: 60px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    h2 {
+        font-size: 80px;
+        line-height: 0;
+    }
+
+    p {
+        width: 75%;
+        line-height: 1;
+    }
+
+</style>
+
 <main>
-    <p>Chapter 1</p>
+    <section class="chapter-two" style="background-color: {triad[0]};">
+        <h2 style="color: {triad[1]};">Lorem ipsum</h2>
+        <p style="color: {triad[2]};">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a diam vel ligula cursus fermentum. Maecenas quis eros ultricies, vestibulum erat et, consequat ipsum. Quisque varius sem eget ipsum viverra, et dictum sapien laoreet. Curabitur in lobortis orci. Duis ac orci sapien. In ac tortor at massa posuere rutrum. Pellentesque sed ex in lorem feugiat finibus et ut sem. Integer tempor sed dolor in congue. Cras ut lobortis erat. Aliquam erat volutpat. Fusce suscipit efficitur ex cursus rhoncus. Vestibulum libero orci, viverra et arcu ac, pharetra tempor neque.</p>
+    </section>
 </main>
